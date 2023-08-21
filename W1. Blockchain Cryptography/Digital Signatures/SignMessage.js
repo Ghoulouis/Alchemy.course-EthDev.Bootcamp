87dc08a6b4078bd74e35ed1b706dcb9aa38163bd
admin <admin@admins-MacBook-Air.local>
2023-08-21 16:24:57 +0700

    21 7

--- W1. Blockchain Cryptography/Digital Signatures/SignMessage.js
+++ W1. Blockchain Cryptography/Digital Signatures/SignMessage.js
@@ -1,5 +1,5 @@
 const secp = require("ethereum-cryptography/secp256k1");
-const hashMessage = require("./Hash Message");
+const hashMessage = require("./HashMessage");
 
 const PRIVATE_KEY =
   "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
